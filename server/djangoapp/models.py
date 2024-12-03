fa49326932cb2c222a862b467d822abeac76ca04
Riaan-Me <riaan.media@outlook.com>
2024-12-03 11:12:08 +0200

    module5-1 of capstone project commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -16,6 +16,7 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -28,8 +29,11 @@ class CarMake(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -39,10 +43,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                               validators=[
+                                MaxValueValidator(2023),
+                                MinValueValidator(2015)
+                               ])
+
     # Other fields as needed
     def __str__(self):
         return self.name  # Return the name as the string representation
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,21 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#####Previus Link
-#ALLOWED_HOSTS = ['localhost','https://riaanpe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-#CSRF_TRUSTED_ORIGINS = ['https://riaanpe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+# Previus Link
+# ALLOWED_HOSTS = ['localhost','https://riaanpe-8000.
+# theianext-1-labs-prod-misc-tools-us-east-0.proxy.
+# cognitiveclass.ai']
+# CSRF_TRUSTED_ORIGINS = ['https://riaanpe-8000.
+# theianext-1-labs-prod-misc-tools-us-east-0.proxy.
+# cognitiveclass.ai']
 
-ALLOWED_HOSTS = ['localhost','https://riaanpe-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://riaanpe-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'https://riaanpe-8000.'
+                 + 'theiadockernext-1-labs-prod-theiak8s'
+                 + '-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://riaanpe-8000.'
+                        + 'theiadockernext-1-labs-prod'
+                        + '-theiak8s-4-tor01.proxy.'
+                        + 'cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -66,9 +75,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),            
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -98,7 +107,9 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.'
+        + 'password_validation.'
+        + 'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -143,8 +154,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
-    os.path.join(BASE_DIR, 'frontend/build/static'),   
+    os.path.join(BASE_DIR, 'frontend/build/static'),
     ]
-
